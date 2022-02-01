ebfc06e5f87da2a3ff01a53d6faa8e9ccf93cc58
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-02-01 01:56:08 +0100

    Add

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo 'Wersja NodeJS:'
+read nodeversion
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y curl
+sudo apt install -y apache2 php7.4-cli mysql-server
+sudo mysql_secure_installation
+sudo mysql -u root -p
+sudo apt install -y phpmyadmin python3 gimp
+curl -fsSL https://deb.nodesource.com/setup_$nodeversion.x | sudo -E bash -
+sudo apt install -y nodejs
