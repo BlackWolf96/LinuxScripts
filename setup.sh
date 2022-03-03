4d0f00b43ba2f15c147352de2062d85ebc71e280
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-03-03 22:13:40 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -9,5 +9,6 @@ sudo apt install -y apache2 php7.4-cli mysql-server
 sudo mysql_secure_installation
 sudo mysql -u root -p
 sudo apt install -y phpmyadmin python3 gimp
+sudo apt install -y npm
 curl -fsSL https://deb.nodesource.com/setup_$nodeversion.x | sudo -E bash -
 sudo apt install -y nodejs
